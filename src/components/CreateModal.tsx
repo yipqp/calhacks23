36a355841c566bf9069458df039053631f43624d
edennle20 <149033073+edennle20@users.noreply.github.com>
2023-10-28 21:12:56 -0700

    Design components

--- src/components/CreateModal.tsx
+++ src/components/CreateModal.tsx
@@ -5,12 +5,13 @@ const CreateModal = ({ action }: { action: () => void }) => {
     upload: "",
     title: "",
     rating: "",
-    caption: "",
+    entry: "",
   });
 
   return (
-    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-16 bg-slate-100">
-      <form action="" className="flex gap-10 ">
+    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-5 bg-orange-50 rounded-2xl shadow-solid-primary
+    border-2 border-black py-6 px shadow-[10px_10px_0px_0px_#0a0a0a]">
+      <form action="" className="flex gap-9 ">
         <div className="">
           <input
             type="file"
@@ -18,11 +19,11 @@ const CreateModal = ({ action }: { action: () => void }) => {
             onChange={(e) => {
               setEntryData({ ...entryData, upload: e.target.value });
             }}
-            className="w-full h-full bg-slate-200"
+            className="w-full h-full bg-white rounded-2xl"
           />
         </div>
         <div className="flex flex-col gap-4">
-          <label className="flex flex-col gap-3">
+          <label className="flex flex-col gap-3 font-inter text-base text-black">
             Title
             <input
               type="text"
@@ -32,7 +33,7 @@ const CreateModal = ({ action }: { action: () => void }) => {
               }}
             />
           </label>
-          <label className="flex flex-col gap-3">
+          <label className="flex flex-col gap-3 font-inter text-base text-black ">
             Rating
             <input
               type="range"
@@ -46,31 +47,31 @@ const CreateModal = ({ action }: { action: () => void }) => {
               }}
             />
           </label>
-          <label className="flex flex-col gap-3">
-            Caption
+          <label className="flex flex-col gap-3 font-inter text-base text-black ">
+            Entry
             <textarea
               name=""
               id=""
               cols="30"
               rows="10"
-              value={entryData.caption}
+              value={entryData.entry}
               onChange={(e) => {
-                setEntryData({ ...entryData, caption: e.target.value });
+                setEntryData({ ...entryData, entry: e.target.value });
               }}
             ></textarea>
           </label>
           <div className="flex justify-between gap-5">
             <button
-              className="border border-gray-500 rounded-md py-1 px-5 flex-1 hover:bg-neutral-800 hover:text-slate-50"
+              className="border border-green-900 rounded-md py-1 px-5 flex-1 hover:bg-black hover:text-slate-50"
               onClick={action}
             >
-              cancel
+              Cancel
             </button>
             <button
-              className="border border-gray-500 rounded-md py-1 px-5 flex-1 hover:bg-neutral-800 hover:text-slate-50"
+              className="border border-green-900 rounded-md py-1 px-5 flex-1 hover:bg-black hover:text-slate-50"
               onClick={action}
             >
-              save
+              Save
             </button>
           </div>
         </div>
